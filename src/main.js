39b372eab46475a8f26e8ef47a07718f306ea809
twx0504 <weixintoo2000@gmail.com>
2025-01-13 11:35:25 +0800

    add js dynamically

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import "./style.css";
+import data from "./data.json";
+
+const dlElement = document.querySelector(".summary-card");
+const docFrag = document.createDocumentFragment();
+const html = String.raw;
+function setData() {
+  data.forEach((it, idx) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "summary-item-wrapper");
+    /* html tag here is for prettier to work. */
+    div.innerHTML = html`
+      <dt class="summary-item">
+        <img
+          width="20"
+          height="20"
+          src=${it.icon}
+          alt="A ${it.category.toLowerCase()} logo."
+        />
+        <h3>${it.category}</h3>
+      </dt>
+      <dd><span class="summary-score-value">${it.score}</span> / 100</dd>
+    `;
+    docFrag.appendChild(div);
+  });
+  dlElement.appendChild(docFrag);
+}
+setData();
